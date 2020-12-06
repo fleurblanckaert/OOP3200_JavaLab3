b839b1af4d86d4f24c3387c2c3d4cd3da9513502
Fleur Blanckaert <fleur.blanckaert@dcmail.ca>
2020-12-06 11:37:44 -0500

    Updated Main

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -19,6 +19,82 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ExtentedWorkTicket ticket1 = new ExtentedWorkTicket(12, "Hello", 12, 12, 2020, "Error", true);
+        ExtentedWorkTicket ticket2 = new ExtentedWorkTicket();
+        ExtentedWorkTicket ticket3 = new ExtentedWorkTicket(ticket1, true);
+
+        // Output Header
+        System.out.println("\n=====================================");
+        System.out.println("IT Support Application: Constructors");
+        System.out.println("=====================================\n");
+
+        System.out.println("\nThe Parameterized constructor" + ticket1.toString());
+        System.out.println("\nThe Parameterized constructor that accepts an object" + ticket3.toString());
+        System.out.println("\nThe Default constructor" + ticket2.toString());
+
+        // Declarations
+        int ticketInput = 0;
+        String clientInput = "";
+        int dayInput = 0;
+        int monthInput = 0;
+        int yearInput = 0;
+        String descriptionInput = "";
+        boolean isValid = true;
+        int i = 1;
+
+        do {
+            try {
+
+                // Output Header
+                System.out.println("\n=====================================");
+                System.out.println("\tIT Support Application: Entry " + i );
+                System.out.println("=====================================\n");
+
+                Scanner keyboard = new Scanner(System.in);
+                //Prompts user to enter ticket ID
+                System.out.print("The Ticket Number is: ");
+                ticketInput = keyboard.nextInt();
+                keyboard.nextLine();
+
+                //Prompts user to enter ticket day
+                System.out.println("The Work Ticket Date is: ");
+                System.out.print("Day: ");
+                dayInput = keyboard.nextInt();
+
+                //Prompts user to enter ticket month
+                System.out.print("Month: ");
+                monthInput = keyboard.nextInt();
+
+                //Prompts user to enter ticket year
+                System.out.print("Year: ");
+                yearInput = keyboard.nextInt();
+                keyboard.nextLine();
+
+                //Prompts user to enter client id
+                System.out.print("The Client ID is: ");
+                clientInput = keyboard.nextLine();
+
+
+                //Prompts user to enter ticket description
+                System.out.print("The Issue Description is: ");
+                descriptionInput = keyboard.nextLine();
+
+                // Displays the output
+                ticket1.SetWorkTicket(ticketInput, clientInput, dayInput, monthInput, yearInput, descriptionInput);
+                System.out.println(ticket1.toString());
+
+
+            } catch (IllegalArgumentException illegalArgumentException) {
+                System.out.println("Exception occurred: " + illegalArgumentException.getMessage());
+                isValid = false;
+
+            } catch (DateTimeException dateTimeException) {
+                System.out.println("Exception occurred: " + dateTimeException.getMessage());
+                isValid = false;
+            }
+            i++;
+        }
+        while(!isValid);
 
     }
 }
